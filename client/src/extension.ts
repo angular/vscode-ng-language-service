0b0116e1e4513476c4b98b97f69ad1bb71b625e9
Chuck Jazdzewski <chuckj@google.com>
2016-09-01 10:20:36 -0700

    WIP: Angular 2 language service

---
+++ client/src/extension.ts
@@ -0,0 +1,41 @@
+/// <reference path="../node_modules/vscode/typings/index.d.ts" />
+
+import * as path from 'path';
+
+import { workspace, Disposable, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
+
+export function activate(context: ExtensionContext) {
+	// The server is implemented in node
+	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
+	// The debug options for the server
+	let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
+	
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	let serverOptions: ServerOptions = {
+		run : { module: serverModule, transport: TransportKind.ipc },
+		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+	}
+	
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: ['ng-template'],
+		synchronize: {
+			fileEvents: [
+				workspace.createFileSystemWatcher('**/tsconfig.json'),
+				workspace.createFileSystemWatcher('**/*.ts')
+			],
+			textDocumentFilter: document => document.fileName.endsWith('.ts') 
+		},
+	}
+	
+	// Create the language client and start the client.
+	let disposable = new LanguageClient('Angular Language Service', serverOptions, clientOptions, true).start();
+	
+	// Push the disposable to the context's subscriptions so that the 
+	// client can be deactivated on extension deactivation
+	context.subscriptions.push(disposable);
+}
+ 

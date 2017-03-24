10251cfa20cbcd1a15f8775707b052da8b64f739
Chuck Jazdzewski <chuckj@google.com>
2017-03-24 10:25:15 -0700

    ci: remove unnecessary travis folding

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -2,44 +2,33 @@
 
 set -ex -o pipefail
 
-echo 'travis_fold:start:BUILD'
-
 # Setup the environment
 cd $(dirname $0)
 source ./env.sh
 cd ../..
 
 # Build the server
-echo 'travis_fold:start:build.server'
 (
   cd server
   yarn run compile
 )
-echo 'travis_fold:end:build.server'
 
 # Build the client
-echo 'travis_fold:start:build.client'
 (
   cd client
   $(yarn bin)/tsc
 )
-echo 'travis_fold:end:build.client'
 
 # Install server production dependencies
-echo 'travis_fold:start:build.server.deps'
 (
   cd client/server
   yarn install --production
 )
-echo 'travis_fold:end:build.server.deps'
 
 # Build the .vsix file
-echo 'travis_fold:start:build.vsix'
 (
-  mkdir dist
+  rm -rf dist && mkdir dist
   cd client
+
   vsce package --out ../dist/ngls.vsix
 )
-echo 'travis_fold:end:build.vsix'
-
-echo 'travis_fold:end:BUILD'
--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -2,32 +2,22 @@
 
 set -ex -o pipefail
 
-echo 'travis_fold:start:INSTALL'
-
 # Setup the environment
 cd $(dirname $0)
 source ./env.sh
 cd ../..
 
 # Install vsce
-echo 'travis_fold:start:install.vsce'
 yarn global add vsce
-echo 'travis_fold:end:install.vsce'
 
 # Install all npm server dependencies
-echo 'travis_fold:start:install.server.node_modules'
 (
   cd server
   yarn
 )
-echo 'travis_fold:end:install.server.node_modules'
 
 # Install all npm client dependencies
-echo 'travis_fold:start:install.client.node_modules'
 (
   cd client
   yarn
 )
-echo 'travis_fold:end:install.client.node_modules'
-
-echo 'travis_fold:end:INSTALL'

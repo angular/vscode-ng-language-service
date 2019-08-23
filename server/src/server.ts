fc255cbcc2e984e4ed5a0872a469cca3f87d21c0
Keen Yee Liau <kyliau@google.com>
2019-08-23 13:00:31 -0700

    feat: Remove legacy code, rewrote server.ts
    This commit replaces the implementation for the language server to use
    the new ProjectService.
    Old tests that are no longer relevent are removed.
    
    This completes the migration to the new prototype.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,279 +1,243 @@
-/* --------------------------------------------------------------------------------------------
- * Portions Copyright (c) Microsoft Corporation. All rights reserved.
- * Portions Copyright (c) Google Inc. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
-import * as ng from '@angular/language-service';
-import * as ts from 'typescript';
 import * as lsp from 'vscode-languageserver';
+import * as ts from 'typescript/lib/tsserverlibrary'; // used as value
+import {createLogger} from './logger';
+import {ServerHost} from './server_host';
+import {ProjectService} from './project_service';
+import {uriToFilePath, filePathToUri, tsTextSpanToLspRange, lspRangeToTsPositions, lspPositionToTsPosition} from './utils';
+import {tsCompletionEntryToLspCompletionItem} from './completion';
 
-import {TextDocuments, TextDocumentEvent, fileNameToUri} from './documents';
-import {ErrorCollector} from './errors';
+enum LanguageId {
+  TS = 'typescript',
+  HTML = 'html',
+}
 
-import {Completion} from '@angular/language-service';
+// Parse startup options
+const options = new Map<string, string>();
+for (let i = 0; i < process.argv.length; ++i) {
+  const argv = process.argv[i];
+  if (argv === '--logFile') {
+    options.set('logFile', process.argv[i + 1]);
+  }
+  if (argv === '--logVerbosity') {
+    options.set('logVerbosity', process.argv[i + 1]);
+  }
+}
 
 // Create a connection for the server. The connection uses Node's IPC as a transport
 const connection: lsp.IConnection = lsp.createConnection();
 
-// Create a simple text document manager. The text document manager
-// supports full document sync only
-const documents: TextDocuments = new TextDocuments(handleTextEvent);
+// logger logs to file. OK to emit verbose entries.
+const logger = createLogger(options);
+// ServerHost provides native OS functionality
+const serverHost = new ServerHost();
+// Our ProjectService is just a thin wrapper around TS's ProjectService
+const projSvc = new ProjectService(serverHost, logger, connection, options);
+const {tsProjSvc} = projSvc;
 
-
-// Setup the error collector that watches for document events and requests errors
-// reported back to the client
-const errorCollector = new ErrorCollector(documents, connection);
-
-function handleTextEvent(event: TextDocumentEvent) {
-  switch (event.kind) {
-    case 'context':
-    case 'change':
-    case 'opened':
-      errorCollector.requestErrors(event.document);
-  }
+// Log initialization info
+connection.console.info(`Log file: ${logger.getLogFileName()}`);
+if (process.env.NG_DEBUG) {
+  logger.info("Angular Language Service is under DEBUG mode");
+}
+if (process.env.TSC_NONPOLLING_WATCHER !== 'true') {
+  connection.console.warn(`Using less efficient polling watcher. Set TSC_NONPOLLING_WATCHER to true.`);
 }
 
-// Make the text document manager listen on the connection
-// for open, change and close text document events
-documents.listen(connection);
-
-// After the server has started the client sends an initilize request. The server receives
-// in the passed params the rootPath of the workspace plus the client capabilites.
-let workspaceRoot: string;
-connection.onInitialize((params): lsp.InitializeResult => {
-  workspaceRoot = params.rootPath;
+// After the server has started the client sends an initilize request.
+connection.onInitialize((params: lsp.InitializeParams): lsp.InitializeResult => {
   return {
     capabilities: {
-      // Tell the client that the server works in FULL text document sync mode
-      textDocumentSync: documents.syncKind,
-      // Tell the client that the server support code complete
+      textDocumentSync: lsp.TextDocumentSyncKind.Incremental,
       completionProvider: {
+        /// The server does not provide support to resolve additional information
+        // for a completion item.
         resolveProvider: false,
         triggerCharacters: ['<', '.', '*', '[', '(']
       },
       definitionProvider: true,
-      hoverProvider: true
-    }
+      hoverProvider: true,
+    },
   }
 });
 
-function compiletionKindToCompletionItemKind(kind: string): lsp.CompletionItemKind {
-  const {CompletionItemKind} = lsp;
-  switch (kind) {
-  case 'attribute': return CompletionItemKind.Property;
-  case 'html attribute': return CompletionItemKind.Property;
-  case 'component': return CompletionItemKind.Class;
-  case 'element': return CompletionItemKind.Class;
-  case 'entity': return CompletionItemKind.Text;
-  case 'key': return CompletionItemKind.Class;
-  case 'method': return CompletionItemKind.Method;
-  case 'pipe': return CompletionItemKind.Function;
-  case 'property': return CompletionItemKind.Property;
-  case 'type': return CompletionItemKind.Interface;
-  case 'reference': return CompletionItemKind.Variable;
-  case 'variable': return CompletionItemKind.Variable;
+connection.onDidOpenTextDocument((params: lsp.DidOpenTextDocumentParams) => {
+  const {uri, text, languageId} = params.textDocument;
+  const filePath = uriToFilePath(uri);
+  if (!filePath) {
+    return;
   }
-  return CompletionItemKind.Text;
-}
 
-const wordRe = /(\w|\(|\)|\[|\]|\*|\-|\_|\.)+/g;
-const special = /\(|\)|\[|\]|\*|\-|\_|\./;
+  const scriptKind = languageId === LanguageId.TS ? ts.ScriptKind.TS : ts.ScriptKind.External;
+  const result = tsProjSvc.openClientFile(filePath, text, scriptKind);
 
-// Convert attribute names with non-\w chracters into a text edit.
-function insertionToEdit(range: lsp.Range, insertText: string): lsp.TextEdit {
-  if (insertText.match(special) && range) {
-    return lsp.TextEdit.replace(range, insertText);
+  const {configFileName, configFileErrors} = result;
+  if (configFileErrors && configFileErrors.length) {
+    connection.console.error(configFileErrors.map(e => e.messageText).join('\n'));
   }
-}
+  if (!configFileName) {
+    // TODO: This could happen if the first file opened is HTML. Fix this.
+    connection.console.error(`No config file for ${filePath}`);
+    return;
+  }
+  const project = tsProjSvc.findProject(configFileName);
+  if (!project) {
+    connection.console.error(`Failed to find project for ${filePath}`);
+    return;
+  }
+  project.markAsDirty();	// Must mark project as dirty to rebuild the program.
+  project.refreshDiagnostics();	// Show initial diagnostics
+});
 
-function getReplaceRange(document: lsp.TextDocumentIdentifier, offset: number): lsp.Range {
-  const line = documents.getDocumentLine(document, offset);
-  if (line && line.text && line.start <= offset && line.start + line.text.length >= offset) {
-    const lineOffset = offset - line.start - 1;
+connection.onDidCloseTextDocument((params: lsp.DidCloseTextDocumentParams) => {
+  const {textDocument} = params;
+  const filePath = uriToFilePath(textDocument.uri);
+  if (!filePath) {
+    return;
+  }
+  tsProjSvc.closeClientFile(filePath);
+});
 
-    // Find the word that contains the offset
-    let found: number, len: number;
-    line.text.replace(wordRe, <any>((word: string, _: string, wordOffset: number) => {
-      if (wordOffset <= lineOffset && wordOffset + word.length >= lineOffset && word.match(special)) {
-        found = wordOffset;
-        len = word.length;
-      }
-    }));
-    if (found != null) {
-      return lsp.Range.create(line.line - 1, found, line.line - 1, found + len);
+connection.onDidChangeTextDocument((params: lsp.DidChangeTextDocumentParams) => {
+  const {contentChanges, textDocument} = params;
+  const filePath = uriToFilePath(textDocument.uri);
+  if (!filePath) {
+    return;
+  }
+  const scriptInfo = tsProjSvc.getScriptInfo(filePath);
+  if (!scriptInfo) {
+    connection.console.log(`Failed to get script info for ${filePath}`);
+    return;
+  }
+  for (const change of contentChanges) {
+    if (change.range) {
+      const [start, end] = lspRangeToTsPositions(scriptInfo, change.range);
+      scriptInfo.editContent(start, end, change.text);
     }
   }
-}
 
-function insertTextOf(completion: Completion): string {
-  switch (completion.kind) {
-    case 'attribute':
-    case 'html attribute':
-      return `${completion.name}=`;
+  const project = scriptInfo.getDefaultProject();
+  project.refreshDiagnostics();
+});
+
+connection.onDidSaveTextDocument((params: lsp.DidSaveTextDocumentParams) => {
+  const {text, textDocument} = params;
+  const filePath = uriToFilePath(textDocument.uri);
+  const scriptInfo = tsProjSvc.getScriptInfo(filePath);
+  if (!scriptInfo) {
+    return;
   }
-  return completion.name;
-}
+  if (text) {
+    scriptInfo.open(text);
+  }
+  else {
+    scriptInfo.reloadFromFile();
+  }
+});
+
+connection.onDefinition((params: lsp.TextDocumentPositionParams) => {
+  const {position, textDocument} = params;
+  const filePath = uriToFilePath(textDocument.uri);
+  const scriptInfo = tsProjSvc.getScriptInfo(filePath);
+  if (!scriptInfo) {
+    connection.console.log(`Script info not found for ${filePath}`);
+    return;
+  }
+
+  const {fileName} = scriptInfo;
+  const project = projSvc.getDefaultProjectForScriptInfo(scriptInfo);
+  if (!project) {
+    return;
+  }
+
+  const offset = lspPositionToTsPosition(scriptInfo, position);
+  const langSvc = project.getLanguageService();
+  const definition = langSvc.getDefinitionAndBoundSpan(fileName, offset);
+  if (!definition || !definition.definitions) {
+    return;
+  }
+  const results: lsp.Location[] = [];
+  for (const d of definition.definitions) {
+    const scriptInfo = tsProjSvc.getScriptInfo(d.fileName);
+    if (!scriptInfo) {
+      continue;
+    }
+    const uri = filePathToUri(d.fileName);
+    const range = tsTextSpanToLspRange(scriptInfo, d.textSpan);
+    results.push(lsp.Location.create(uri, range));
+  }
+  return results;
+});
+
+connection.onHover((params: lsp.TextDocumentPositionParams) => {
+  const {position, textDocument} = params;
+  const filePath = uriToFilePath(textDocument.uri);
+  if (!filePath) {
+    return;
+  }
+  const scriptInfo = tsProjSvc.getScriptInfo(filePath);
+  if (!scriptInfo) {
+    return;
+  }
+  const {fileName} = scriptInfo;
+  const project = tsProjSvc.getDefaultProjectForFile(fileName, true /* ensureProject */);
+  if (!project) {
+    return;
+  }
+  const offset = lspPositionToTsPosition(scriptInfo, position);
+  const langSvc = project.getLanguageService();
+  const info = langSvc.getQuickInfoAtPosition(fileName, offset);
+  if (!info) {
+    return;
+  }
+  const {kind, kindModifiers, textSpan, displayParts, documentation} = info;
+  let desc = kindModifiers ? kindModifiers + ' ' : '';
+  if (displayParts) {
+    // displayParts does not contain info about kindModifiers
+    // but displayParts does contain info about kind
+    desc += displayParts.map(dp => dp.text).join('');
+  }
+  else {
+    desc += kind;
+  }
+  const contents: lsp.MarkedString[] = [{
+    language: 'typescript',
+    value: desc,
+  }];
+  if (documentation) {
+    for (const d of documentation) {
+      contents.push(d.text);
+    }
+  }
+  return {
+    contents,
+    range: tsTextSpanToLspRange(scriptInfo, textSpan),
+  };
+});
 
 // This handler provides the initial list of the completion items.
-connection.onCompletion((params: lsp.TextDocumentPositionParams): lsp.CompletionItem[] => {
+connection.onCompletion((params: lsp.CompletionParams) => {
   const {position, textDocument} = params;
-  const {fileName, service, offset, languageId} = documents.getServiceInfo(textDocument, position);
-  if (fileName && service && offset != null) {
-    let result = service.getCompletionsAt(fileName, offset);
-    if (!result) {
-      return;
-    }
-    if (Array.isArray(result)) {  // old ng.Completion[]
-      if (languageId == 'html') {
-        // The HTML elements are provided by the HTML service when the text type is 'html'.
-        result = result.filter(completion => completion.kind != 'element');
-      }
-      const replaceRange = getReplaceRange(params.textDocument, offset);
-      return result.map(completion => ({
-        label: completion.name,
-        kind: compiletionKindToCompletionItemKind(completion.kind),
-        detail: completion.kind,
-        sortText: completion.sort,
-        textEdit: insertionToEdit(replaceRange, insertTextOf(completion)),
-        insertText: insertTextOf(completion)
-      }));
-    }
-    const entries = (result as ts.CompletionInfo).entries;
-    return entries.map(entry => {
-      const item = lsp.CompletionItem.create(entry.name);
-      item.kind = compiletionKindToCompletionItemKind(entry.kind);
-      item.detail = entry.kind;
-      item.sortText = entry.sortText;
-      item.textEdit = lsp.TextEdit.insert(position, entry.name);
-      return item;
-    });
+  const filePath = uriToFilePath(textDocument.uri);
+  if (!filePath) {
+    return;
   }
+  const scriptInfo = tsProjSvc.getScriptInfo(filePath);
+  if (!scriptInfo) {
+    return;
+  }
+  const project = projSvc.getDefaultProjectForScriptInfo(scriptInfo);
+  if (!project) {
+    return;
+  }
+  const offset = lspPositionToTsPosition(scriptInfo, position);
+  const langSvc = project.getLanguageService();
+  const completions = langSvc.getCompletionsAtPosition(filePath, offset, {
+    // options
+  });
+  if (!completions || !completions.entries.length) {
+    return;
+  }
+  return completions.entries.map((e) => tsCompletionEntryToLspCompletionItem(e, position));
 });
 
-function ngDefinitionToDefinition(definition: ng.Definition): lsp.Definition {
-  const locations = definition.map(d => {
-    const document = lsp.TextDocumentIdentifier.create(fileNameToUri(d.fileName));
-    const positions = documents.offsetsToPositions(document, [d.span.start, d.span.end]);
-    return {document, positions}
-  }).filter(d => d.positions.length > 0).map(d => {
-    const range = lsp.Range.create(d.positions[0], d.positions[1]);
-    return lsp.Location.create(d.document.uri, range);
-  });
-  if (locations && locations.length) {
-    return locations;
-  }
-}
-
-function logErrors<T>(f: () => T): T {
-  try {
-    return f();
-  } catch (e) {
-    if (e.message && e.stack) connection.console.error(`SERVER ERROR: ${e.message}\n${e.stack}`);
-    throw e;
-  }
-}
-
-connection.onDefinition((params: lsp.TextDocumentPositionParams) => logErrors(() => {
-  const {textDocument, position} = params;
-  const {fileName, service, offset} = documents.getServiceInfo(textDocument, position);
-  if (fileName && service && offset != null) {
-    let result = service.getDefinitionAt(fileName, offset);
-    if (!result) {
-      return;
-    }
-    if (Array.isArray(result)) {
-      // Backwards compatibility with old ng.Location[] (ng.Definition)
-      return ngDefinitionToDefinition(result);
-    }
-    const {textSpan, definitions} = result as ts.DefinitionInfoAndBoundSpan;
-    if (!definitions || !definitions.length) {
-      return;
-    }
-    const [start, end] = documents.offsetsToPositions(textDocument, [
-      textSpan.start,
-      textSpan.start + textSpan.length,
-    ]);
-    const originSelectionRange = lsp.Range.create(start, end);
-    return definitions.map((d) => {
-      const targetUri = lsp.TextDocumentIdentifier.create(fileNameToUri(d.fileName));
-      const [start, end] = documents.offsetsToPositions(targetUri, [
-        d.textSpan.start,
-        d.textSpan.start + d.textSpan.length,
-      ]);
-      const targetRange = lsp.Range.create(start, end);
-      return {
-        originSelectionRange,
-        targetUri : targetUri.uri,
-        targetRange,
-        targetSelectionRange: targetRange,
-      };
-    });
-  }
-}));
-
-function ngHoverToHover(hover: ng.Hover, document: lsp.TextDocumentIdentifier): lsp.Hover {
-  if (hover) {
-    const positions = documents.offsetsToPositions(document, [hover.span.start, hover.span.end]);
-    if (positions) {
-      const range = lsp.Range.create(positions[0], positions[1]);
-      return {
-        contents: {language: 'typescript', value: hover.text.map(t => t.text).join('')},
-        range
-      };
-    }
-  }
-}
-
-connection.onHover((params: lsp.TextDocumentPositionParams): lsp.Hover => logErrors(() => {
-  const {position, textDocument} = params;
-  const {fileName, service, offset} = documents.getServiceInfo(textDocument, position);
-  if (fileName && service && offset != null) {
-    const result = service.getHoverAt(fileName, offset);
-    if (!result) {
-      return;
-    }
-    if (isNgHover(result)) {
-      // Backwards compatibility with old ng.Hover
-      return ngHoverToHover(result, params.textDocument);
-    }
-    const {kind, kindModifiers, textSpan, displayParts, documentation} = result as ts.QuickInfo;
-    let desc = kindModifiers ? kindModifiers + ' ' : '';
-    if (displayParts) {
-      // displayParts does not contain info about kindModifiers
-      // but displayParts does contain info about kind
-      desc += displayParts.map(dp => dp.text).join('');
-    }
-    else {
-      desc += kind;
-    }
-    const contents: lsp.MarkedString[] = [{
-      language: 'typescript',
-      value: desc,
-    }];
-    if (documentation) {
-      for (const d of documentation) {
-        contents.push(d.text);
-      }
-    }
-    const [start, end] = documents.offsetsToPositions(textDocument, [
-      textSpan.start,
-      textSpan.start + textSpan.length,
-    ]);
-    return {
-      contents,
-      range: lsp.Range.create(
-        start.line - 1, start.character - 1, end.line - 1, end.character - 1),
-    };
-  }
-}));
-
-function isNgHover(result: ng.Hover): result is ng.Hover {
-  if (result.span && Array.isArray(result.text)) {
-    return true;
-  }
-  return false;
-}
-
-// Listen on the connection
 connection.listen();

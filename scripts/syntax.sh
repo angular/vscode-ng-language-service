baa45d3cf9ae39de08d0c9e3654aba8d702881e4
ayazhafiz <ayaz.hafiz.1@gmail.com>
2019-12-17 11:53:15 -0600

    refactor: rename template syntax to inline_template
    This commit performs a minor refactor, changing the TextMate inline
    template syntax file name and scopes to explicitly be `inline_template`.
    This is in preparation for adding a TextMate grammar for CSS styles in
    `@Component` `styles` properties.

--- scripts/syntax.sh
+++ scripts/syntax.sh
@@ -9,7 +9,7 @@ set -ex -o pipefail
 
 DUMMY_GRAMMARS=$(find syntaxes/test -name '*-dummy.json' -exec echo "-g {}" \; | tr '\n' ' ')
 ARGS=$(cat<<ARGS
-  -s template.ng 
+  -s inline-template.ng 
   -g syntaxes/template.ng.json $DUMMY_GRAMMARS 
   -t syntaxes/test/**/*.ts
 ARGS

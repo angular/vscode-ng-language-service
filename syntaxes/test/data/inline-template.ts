0076e2e89dab7b3759244095751c921c6e305ac4
hafiz <ayaz.hafiz.1@gmail.com>
2019-12-18 16:02:20 -0600

    feat: add syntax highlighting grammar for interpolations (#516)
    * feat: add syntax highlighting grammar for interpolations
    
    Adds a grammar for Angular interpolations. Because the grammar relies
    only on regex expressions, we cannot currently support any interpolation
    delimiters other than the default `{{`/`}}` -- to do more would require
    a more sophisticated highlighting solution.
    
    For convinience, everything inside an interpolation is treated as
    JavaScript syntax, even though the expressions available in an Angular
    interpolations are a distinct subset of JavaScript.
    
    Attached to this PR are several before/after images of this change.
    
    Partially addresses #483.
    
    * fixup! feat: add syntax highlighting grammar for interpolations

--- syntaxes/test/data/inline-template.ts
+++ syntaxes/test/data/inline-template.ts
@@ -1,18 +1,19 @@
 /* clang-format off */
 
+/* Inline template recognition tests */
 @Component({
-//// Property key/value test
+  //// Property key/value test
   template: '<div></div>',
 
-//// String delimiter tests
+  //// String delimiter tests
   template: `<div></div>`,
   template: "<div></div>",
   template: '<div></div>',
 
-//// Parenthesization tests
+  //// Parenthesization tests
   template: ( (( '<div></div>' )) ),
 
-//// Comments tests
+  //// Comments tests
   // template: '<div></div>'
   /*
    * template: '<div></div>'
@@ -20,5 +21,12 @@
   /**
    * template: '<div></div>'
    */
+})
+export class TMComponent{}
+
+/* Template syntax tests */
+@Component({
+  // Interpolation test
+  template: '{{property}}',
 })
 export class TMComponent{}

c4b5bea2fb2ca00c98ded07e1796f21cb21e3417
dannymcgee <dannymcgee@gmail.com>
2020-01-26 14:51:01 -0500

    Refactored to function declarations

--- syntaxes/src/build.ts
+++ syntaxes/src/build.ts
@@ -12,34 +12,34 @@ import {GrammarDefinition, JsonObject} from './types';
 
 import {template} from './template';
 
-const transformValue = (value: string|RegExp|GrammarDefinition|GrammarDefinition[]): string|
-    JsonObject|JsonObject[] => {
-      if (typeof value === 'string') {
-        return value;
-      } else if (value instanceof RegExp) {
-        return value.toString().replace(/^\/|\/$/g, '');
-      } else if (value instanceof Array) {
-        return value.map(processGrammar);
-      }
-      return processGrammar(value);
-    };
+function transformValue(value: string|RegExp|GrammarDefinition|GrammarDefinition[]): string|
+    JsonObject|JsonObject[] {
+  if (typeof value === 'string') {
+    return value;
+  } else if (value instanceof RegExp) {
+    return value.toString().replace(/^\/|\/$/g, '');
+  } else if (value instanceof Array) {
+    return value.map(processGrammar);
+  }
+  return processGrammar(value);
+}
 
-const processGrammar = (grammar: GrammarDefinition): JsonObject => {
+function processGrammar(grammar: GrammarDefinition): JsonObject {
   const processedGrammar: JsonObject = {};
   for (const [key, value] of Object.entries(grammar)) {
     processedGrammar[key] = transformValue(value);
   }
 
   return processedGrammar;
-};
+}
 
-const build = (grammar: GrammarDefinition, filename: string): void => {
+function build(grammar: GrammarDefinition, filename: string): void {
   const processedGrammar: JsonObject = processGrammar(grammar);
   const grammarContent: string = JSON.stringify(processedGrammar, null, '  ') + '\n';
 
   fs.writeFile(`syntaxes/${filename}.json`, grammarContent, (error) => {
     if (error) throw error;
   });
-};
+}
 
 build(template, 'template');

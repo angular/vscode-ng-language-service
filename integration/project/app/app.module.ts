cd493f716a4b6fb8b82f1a9bffdd35a5ad387071
Keen Yee Liau <kyliau@umich.edu>
2020-09-21 13:46:03 -0700

    test: add @angular/common to test project
    Install `@angular/common` in the test project so that we could test
    features like pipes.
    
    `@angular/compiler` and `@angular/compiler-cli` are needed so that
    we can run ngcc.

--- integration/project/app/app.module.ts
+++ integration/project/app/app.module.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule }      from '@angular/core';
 import { AppComponent }  from './app.component';
 import { FooComponent } from './foo.component';
 
 @NgModule({
-  imports:      [],
+  imports:      [
+    CommonModule,
+  ],
   declarations: [
     AppComponent,
     FooComponent,

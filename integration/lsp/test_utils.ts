0f9e82ca9c9a792d7d2fd6c764f059552de64a3a
Keen Yee Liau <kyliau@umich.edu>
2020-11-05 11:45:08 -0800

    test: refactor integration test for LSP
    It is not always straight-forward to write unit tests for the code in
    this repo because of the nature of the language server.
    
    However, since the language server uses JSON-RPC, it makes sense to test
    the server along this boundary through stateful integration test.
    
    This PR makes writing integration tests a lot easier by refactoring the
    test client. As a result, we can now
    
    1. Use strongly typed requests and responses.
    2. Have much better abstraction of the test client.
    3. Easily write more tests!
    
    This refactoring is done in preparation for the upcoming ngcc feature.

---
+++ integration/lsp/test_utils.ts
@@ -0,0 +1,76 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {fork} from 'child_process';
+import * as fs from 'fs';
+import {resolve} from 'path';
+import {createMessageConnection, IPCMessageReader, IPCMessageWriter, MessageConnection} from 'vscode-jsonrpc';
+import * as lsp from 'vscode-languageserver-protocol';
+
+const SERVER_PATH = resolve(__dirname, '../../../dist/server/index.js');
+const PACKAGE_ROOT = resolve(__dirname, '../../..');
+const PROJECT_PATH = resolve(__dirname, '../../project');
+export const APP_COMPONENT = `${PROJECT_PATH}/app/app.component.ts`;
+export const FOO_TEMPLATE = `${PROJECT_PATH}/app/foo.component.html`;
+
+export interface ServerOptions {
+  ivy: boolean;
+}
+
+export function createConnection(serverOptions: ServerOptions): MessageConnection {
+  const argv: string[] = [
+    '--node-ipc',
+    '--tsProbeLocations',
+    PACKAGE_ROOT,
+    '--ngProbeLocations',
+    SERVER_PATH,
+  ];
+  if (serverOptions.ivy) {
+    argv.push('--experimental-ivy');
+  }
+  const server = fork(SERVER_PATH, argv, {
+    cwd: PROJECT_PATH,
+    env: {
+      TSC_NONPOLLING_WATCHER: 'true',
+    },
+  });
+
+  return createMessageConnection(
+      new IPCMessageReader(server),
+      new IPCMessageWriter(server),
+  );
+}
+
+export function initializeServer(client: MessageConnection): Promise<lsp.InitializeResult> {
+  return client.sendRequest(lsp.InitializeRequest.type, {
+    /**
+     * The process id of the parent process that started the server. It is
+     * always the current process.
+     */
+    processId: process.pid,
+    rootUri: `file://${PROJECT_PATH}`,
+    capabilities: {},
+    /**
+     * Options are 'off' | 'messages' | 'verbose'.
+     * To debug test failure, set to 'verbose'.
+     */
+    trace: 'off',
+    workspaceFolders: null,
+  });
+}
+
+export function openTextDocument(client: MessageConnection, filePath: string) {
+  client.sendNotification(lsp.DidOpenTextDocumentNotification.type, {
+    textDocument: {
+      uri: `file://${filePath}`,
+      languageId: 'typescript',
+      version: 1,
+      text: fs.readFileSync(filePath, 'utf-8'),
+    },
+  });
+}

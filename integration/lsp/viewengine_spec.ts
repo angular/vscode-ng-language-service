c4e17acbe03145dcc09afbed3e9c4818b5169d50
Keen Yee Liau <kyliau@umich.edu>
2020-11-05 11:45:08 -0800

    test: refactor integration test for LSP
    It is not always straight-forward to write unit tests for the code in
    this repo because of the nature of the language server.
    
    However, since the language server uses JSON-RPC, it makes sense to test
    the server along this boundary through stateful integration test.
    
    This PR makes writing integration tests a lot easier by refactoring the
    test client. As a result, we can now
    
    1. Use strongly typed requests and responses.
    2. Have much better abstraction of the test client.
    3. Easily write more tests!
    
    This refactoring is done in preparation for the upcoming ngcc feature.

---
+++ integration/lsp/viewengine_spec.ts
@@ -0,0 +1,117 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {MessageConnection} from 'vscode-jsonrpc';
+import * as lsp from 'vscode-languageserver-protocol';
+import {APP_COMPONENT, createConnection, FOO_TEMPLATE, initializeServer, openTextDocument} from './test_utils';
+
+describe('Angular language server', () => {
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000; /* 10 seconds */
+
+  let client: MessageConnection;
+
+  beforeEach(async () => {
+    client = createConnection({
+      ivy: false,
+    });
+    client.listen();
+    await initializeServer(client);
+  });
+
+  afterEach(() => {
+    client.dispose();
+  })
+
+  it('should handle completion', async () => {
+    openTextDocument(client, APP_COMPONENT);
+    client.sendNotification(lsp.DidChangeTextDocumentNotification.type, {
+      textDocument: {
+        uri: `file://${APP_COMPONENT}`,
+        version: 2,
+      },
+      contentChanges: [
+        {
+          range: {
+            start: {line: 4, character: 29},
+            end: {line: 4, character: 29},
+          },
+          text: '.',
+        },
+      ],
+    });
+    const response = await client.sendRequest(lsp.CompletionRequest.type, {
+      textDocument: {
+        uri: `file://${APP_COMPONENT}`,
+      },
+      position: {line: 4, character: 30},
+    });
+    expect(response).toContain({
+      label: 'charAt',
+      kind: 2,
+      detail: 'method',
+      sortText: 'charAt',
+      textEdit: {
+        range: {
+          start: {line: 4, character: 30},
+          end: {line: 4, character: 30},
+        },
+        newText: 'charAt()',
+      },
+    });
+  });
+
+  it('should handle hover on external template', async () => {
+    openTextDocument(client, FOO_TEMPLATE);
+    const response = await client.sendRequest(lsp.HoverRequest.type, {
+      textDocument: {
+        uri: `file://${FOO_TEMPLATE}`,
+      },
+      position: {line: 0, character: 3},
+    });
+    expect(response).toEqual({
+      contents: [
+        {
+          language: 'typescript',
+          value: '(property) FooComponent.title: string',
+        },
+      ],
+      range: {
+        start: {line: 0, character: 2},
+        end: {line: 0, character: 7},
+      },
+    });
+  });
+});
+
+describe('initialization', () => {
+  it('should be handled at startup', async () => {
+    const client = createConnection({
+      ivy: false,
+    });
+    client.listen();
+    const response = await initializeServer(client);
+    expect(response).toEqual({
+      capabilities: {
+        textDocumentSync: 2,
+        completionProvider: {
+          resolveProvider: false,
+          triggerCharacters: ['<', '.', '*', '[', '(', '$', '|'],
+        },
+        definitionProvider: true,
+        typeDefinitionProvider: true,
+        hoverProvider: true,
+        workspace: {
+          workspaceFolders: {
+            supported: true,
+          },
+        },
+      },
+    });
+    client.dispose();
+  });
+});

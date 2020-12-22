a03f022ebb4069f1180b98484affd165bdffd761
Keen Yee Liau <kyliau@umich.edu>
2020-12-22 11:07:03 -0800

    test: add multi-project workspace

---
+++ integration/workspace/projects/demo/src/app/app.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+}

f7836ebc1207638d3f6005958801cf7f60b0b8b0
Andrew Scott <atscott01@gmail.com>
2021-03-04 12:11:10 -0800

    perf: prevent requests from being sent to the sesrver when outside Angular context
    The Angular Language Service extension of vscode has several pieces to
    it:
    
    * The client, which communicates directly with vscode
    * The server, which handles LSP requests over an lsp connection
    * @angular/language-service which provides concrete answers to LS queries
    
    We added an optimization the @angular/language-service which exits early
    when a request is outside the Angular context. This prevents unnecessary
    Angular analysis of a file when we know from the start that there are no
    Angular-specific results at a location.
    
    This commit provides an additional optimization by adding a similar preventing
    short-circuit from the client side. This prevents requests from even
    being sent to the server when we know there is no Angular information at
    a location. This optimization is a necessary addition because the server
    can be blocked from processing requests if another one is taking a while
    to respond. We found that requests for diagnostics, which are necessary
    when opening a new file, block other server requests from being
    processed. This commit would prevent that block from happening by never
    making the request to the server in the first place.
    
    fixes #1176

---
+++ client/src/embedded_support.ts
@@ -0,0 +1,94 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import * as ts from 'typescript';
+import * as vscode from 'vscode';
+
+/** Determines if the position is inside an inline template. */
+export function isInsideInlineTemplateRegion(
+    document: vscode.TextDocument, position: vscode.Position): boolean {
+  if (document.languageId !== 'typescript') {
+    return true;
+  }
+  return isPropertyAssignmentToStringOrStringInArray(
+      document.getText(), document.offsetAt(position), ['template']);
+}
+
+/** Determines if the position is inside an inline template, templateUrl, or string in styleUrls. */
+export function isInsideComponentDecorator(
+    document: vscode.TextDocument, position: vscode.Position): boolean {
+  if (document.languageId !== 'typescript') {
+    return true;
+  }
+  return isPropertyAssignmentToStringOrStringInArray(
+      document.getText(), document.offsetAt(position), ['template', 'templateUrl', 'styleUrls']);
+}
+
+/**
+ * Basic scanner to determine if we're inside a string of a property with one of the given names.
+ *
+ * This scanner is not currently robust or perfect but provides us with an accurate answer _most_ of
+ * the time.
+ *
+ * False positives are OK here. Though this will give some false positives for determining if a
+ * position is within an Angular context, i.e. an object like `{template: ''}` that is not inside an
+ * `@Component` or `{styleUrls: [someFunction('stringL¦iteral')]}`, the @angular/language-service
+ * will always give us the correct answer. This helper gives us a quick win for optimizing the
+ * number of requests we send to the server.
+ */
+function isPropertyAssignmentToStringOrStringInArray(
+    documentText: string, offset: number, propertyAssignmentNames: string[]): boolean {
+  const scanner = ts.createScanner(ts.ScriptTarget.ESNext, true /* skipTrivia */);
+  scanner.setText(documentText);
+
+  let token: ts.SyntaxKind = scanner.scan();
+  let lastToken: ts.SyntaxKind|undefined;
+  let lastTokenText: string|undefined;
+  let unclosedBraces = 0;
+  let unclosedBrackets = 0;
+  let propertyAssignmentContext = false;
+  while (token !== ts.SyntaxKind.EndOfFileToken && scanner.getStartPos() < offset) {
+    if (lastToken === ts.SyntaxKind.Identifier && lastTokenText !== undefined &&
+        propertyAssignmentNames.includes(lastTokenText) && token === ts.SyntaxKind.ColonToken) {
+      propertyAssignmentContext = true;
+      token = scanner.scan();
+      continue;
+    }
+    if (unclosedBraces === 0 && unclosedBrackets === 0 && isPropertyAssignmentTerminator(token)) {
+      propertyAssignmentContext = false;
+    }
+
+    if (token === ts.SyntaxKind.OpenBracketToken) {
+      unclosedBrackets++;
+    } else if (token === ts.SyntaxKind.OpenBraceToken) {
+      unclosedBraces++;
+    } else if (token === ts.SyntaxKind.CloseBracketToken) {
+      unclosedBrackets--;
+    } else if (token === ts.SyntaxKind.CloseBraceToken) {
+      unclosedBraces--;
+    }
+
+    const isStringToken = token === ts.SyntaxKind.StringLiteral ||
+        token === ts.SyntaxKind.NoSubstitutionTemplateLiteral;
+    const isCursorInToken = scanner.getStartPos() <= offset &&
+        scanner.getStartPos() + scanner.getTokenText().length >= offset;
+    if (propertyAssignmentContext && isCursorInToken && isStringToken) {
+      return true;
+    }
+
+    lastTokenText = scanner.getTokenText();
+    lastToken = token;
+    token = scanner.scan();
+  }
+
+  return false;
+}
+
+function isPropertyAssignmentTerminator(token: ts.SyntaxKind) {
+  return token === ts.SyntaxKind.EndOfFileToken || token === ts.SyntaxKind.CommaToken ||
+      token === ts.SyntaxKind.SemicolonToken || token === ts.SyntaxKind.CloseBraceToken;
+}

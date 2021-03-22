b8710fb1475695ab7811680001a253eb3a0b14f8
Andrew Scott <atscott01@gmail.com>
2021-03-22 15:55:55 -0700

    build: switch to esbuild for all compilation
    This commit configures the esbuild output of for the banner and server
    to be in a format that will work for the extension. It mimics the sort
    of behavior that we would get in the amd format (which is not supported
    by esbuild) by simply overriding the built-in `require` function with
    the function which is capable of resolving typescript at runtime from
    the location passed in via the arguments.
    
    Fixes #1123

--- server/src/banner.ts
+++ server/src/banner.ts
@@ -1,23 +1,32 @@
 import {parseCommandLine} from './cmdline_utils';
 import {resolveTsServer} from './version_provider';
 
+const originalRequire = require;
+
 /**
- * This method provides a custom implementation for the AMD loader to resolve
- * `typescript` module at runtime.
- * @param modules modules to resolve
- * @param cb function to invoke with resolved modules
+ * This method provides a custom implementation for the require function to resolve
+ * `typescript` module at runtime. We provide this as an override to the built-in
+ * 'require' method using the banner funcionality of esbuild. That is, esbuild will
+ * compile the server and add this banner to the top of the compilation so any place
+ * in the server code that uses `require` will get routed through this override.
+ *
+ * Refer also to `esbuild.js`, the `bannerConfig` which overrides the `require` using
+ * the `footer` option, and the `serverConfig` which provides the banner code at the top
+ * of the server output using the `banner` option.
+ *
+ * @param moduleName The module to resolve
+ * @returns
  */
-export function define(modules: string[], cb: (...modules: any[]) => void) {
+export function requireOverride(moduleName: string) {
   const TSSERVER = 'typescript/lib/tsserverlibrary';
-  const resolvedModules = modules.map(m => {
-    if (m === 'typescript') {
-      throw new Error(`Import '${TSSERVER}' instead of 'typescript'`);
-    }
-    if (m === TSSERVER) {
-      const {tsProbeLocations} = parseCommandLine(process.argv);
-      m = resolveTsServer(tsProbeLocations).resolvedPath;
-    }
-    return require(m);
-  });
-  cb(...resolvedModules);
+  if (moduleName === 'typescript') {
+    throw new Error(`Import '${TSSERVER}' instead of 'typescript'`);
+  }
+  if (moduleName === TSSERVER) {
+    const {tsProbeLocations} = parseCommandLine(process.argv);
+    moduleName = resolveTsServer(tsProbeLocations).resolvedPath;
+  }
+  return originalRequire(moduleName);
 }
+
+requireOverride.resolve = originalRequire.resolve;

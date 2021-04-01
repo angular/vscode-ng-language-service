513ad2a1e456170a91c18eb1dde812804939f778
Joey Perrott <josephperrott.github@gmail.com>
2021-03-31 17:41:18 -0700

    build: set up ng-dev toolset for the repo (#1244)
    Set up the ng-dev toolset for the repo to allow it to be used for merging PRs.

---
+++ .ng-dev/github.ts
@@ -0,0 +1,9 @@
+import {GithubConfig} from '@angular/dev-infra-private/utils/config';
+
+/**
+ * Github configuration for the `ng-dev` toolset.
+ */
+export const github: GithubConfig = {
+  name: 'vscode-ng-language-service',
+  owner: 'angular',
+};

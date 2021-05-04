f4ffd36ca054b6c2650f9f463cf1f0d189cf3e0b
Andrew Scott <atscott01@gmail.com>
2021-05-04 15:22:19 -0700

    fix: remove TSC_NONPOLLING_WATCHER env variable and provide default watchOptions (#1323)
    `TSC_NONPOLLING_WATCHER` was used back in the early days to improve
    file watcher performance, but it prevents files / directories from
    being moved / renamed.
    Since TS now provides built-in option to configure file watcher, this
    environment variable is no longer needed.
    
    See https://www.typescriptlang.org/docs/handbook/configuring-watch.html
    
    The watch options used are derived from the user defined options in
    their `tsconfig` combined with the default options provided to the
    project service via `setHostConfiguration`
    ([code reference](https://github.com/microsoft/TypeScript/blob/f7ef1540d3c10fb282d1d433d9f2850b28391169/src/server/editorServices.ts#L2985-L2989)).
    
    As noted in the documentation for configuration watch options, the
    default when no watch options are provided falls back to `fs.watchFile`
    with `250ms` for any file. This would cause the performance issues as
    seen in #1310.
    
    Fixes #750

--- server/src/server.ts
+++ server/src/server.ts
@@ -56,8 +56,4 @@ if (logger.loggingEnabled()) {
 if (process.env.NG_DEBUG === 'true') {
   session.info('Angular Language Service is running under DEBUG mode');
 }
-if (process.env.TSC_NONPOLLING_WATCHER !== 'true') {
-  session.warn(`Using less efficient polling watcher. Set TSC_NONPOLLING_WATCHER to true.`);
-}
-
 session.listen();

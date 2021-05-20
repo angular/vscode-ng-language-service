e1a7137aa91a7bdccda0f1df3625d17c27c0680e
Andrew Scott <atscott01@gmail.com>
2021-05-20 09:37:42 -0700

    fix: only give html provider completions for inline templates (#1369)
    Our client-side middleware retrieves html provider completions for
    inline templates because the template string is not otherwise detected
    as HTML by the registered HTML providers. We should only create this
    virtual document and retrieve HTML provider results for typescript
    files.
    
    Fixes #1357

--- integration/e2e/helper.ts
+++ integration/e2e/helper.ts
@@ -1,4 +1,11 @@
 import * as vscode from 'vscode';
+import {APP_COMPONENT, FOO_TEMPLATE} from '../test_constants';
+
+export const COMPLETION_COMMAND = 'vscode.executeCompletionItemProvider';
+export const HOVER_COMMAND = 'vscode.executeHoverProvider';
+export const DEFINITION_COMMAND = 'vscode.executeDefinitionProvider';
+export const APP_COMPONENT_URI = vscode.Uri.file(APP_COMPONENT);
+export const FOO_TEMPLATE_URI = vscode.Uri.file(FOO_TEMPLATE);
 
 function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));

87c1b70425ba836520e3be17f00c7fe4d536242f
Andrew Scott <atscott01@gmail.com>
2021-06-08 16:56:09 -0700

    chore(deps): update dependency @types/jasmine to v3.7.6 (#1367)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- integration/e2e/jasmine.ts
+++ integration/e2e/jasmine.ts
@@ -1,14 +1,15 @@
 import Jasmine = require('jasmine');
 
 export async function run(): Promise<void> {
-  const jasmine = new Jasmine({});
+  const jasmine = new Jasmine({projectBaseDir: __dirname});
 
   jasmine.loadConfig({
-    spec_dir: 'dist/integration/e2e',
     spec_files: [
       '*_spec.js',
     ],
   });
+
+  console.log(`Expecting to run ${jasmine.specFiles.length} specs.`);
 
   if (jasmine.specFiles.length === 0) {
     throw new Error('No specs found');

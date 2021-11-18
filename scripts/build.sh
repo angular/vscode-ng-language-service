6adec8f8117092c82efe1f0e632fe6e247647e76
Joey Perrott <josephperrott.github@gmail.com>
2021-11-18 13:00:07 -0800

    ci: setup scheduled CI job to test repository against the latest builds (#1567)
    Use a scheduled job to check the repository against the latest version of @angular/language-service from
    https://github.com/angular/language-services-builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
 
+DEPENDENCY_TYPE=$1
+USE_PACKAGE_JSON=false
+USE_BUILDS_REPO=false
+
+if [ "$DEPENDENCY_TYPE" = "package.json" ]; then
+  USE_PACKAGE_JSON=true
+  echo "Using the dependencies defined in package.json"
+elif [ "$DEPENDENCY_TYPE" = "builds-repo" ]; then
+  USE_BUILDS_REPO=true
+  echo "Use the latest dependency from builds repo."
+else
+  echo "Please provide a valid dependency type as the first argument: 'package.json' or 'builds-repo'"
+  echo
+  echo "Usage example:"
+  echo "  $0 package.json"
+  exit 1;
+fi
+
+if [ "$USE_BUILDS_REPO" = true ]; then
+  for dep in "language-service"; do
+    echo $(node -e "
+        const pkgJson = require('./package.json');
+        pkgJson['dependencies']['@angular/$dep'] = 'https://github.com/angular/$dep-builds.git';
+        console.log(JSON.stringify(pkgJson));
+        ") > package.json
+  done
+fi
+
 set -ex -o pipefail
 
 # Enable extended pattern matching features
@@ -8,6 +36,9 @@ shopt -s extglob
 # Clean up from last build
 rm -rf dist
 rm -rf **/*.tsbuildinfo
+
+
+yarn install;
 
 # Build the client and server
 yarn run compile

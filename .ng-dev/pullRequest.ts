f10e49529acb1126196ab175659b14e9cfcd6fa5
Andrew Scott <atscott01@gmail.com>
2022-04-29 12:51:13 -0700

    fix(dev-infra): fix ng-dev configuration

--- .ng-dev/pullRequest.ts
+++ .ng-dev/pullRequest.ts
@@ -1,18 +1,10 @@
-import {PullRequestConfig, ReleaseConfig} from '@angular/dev-infra-private/ng-dev';
-
-/** Configuration for the `ng-dev release` command, defined here for use by the merge config. */
-export const release: ReleaseConfig = {
-  representativeNpmPackage: '@angular/language-server',
-  npmPackages: [{name: '@angular/language-server'}],
-  buildPackages: async () => [],
-};
-
+import {PullRequestConfig} from '@angular/dev-infra-private/ng-dev';
 
 /**
  * Configuration for the merge tool in `ng-dev`. This sets up the labels which
  * are respected by the merge script (e.g. the target labels).
  */
-export const merge: PullRequestConfig = {
+export const pullRequest: PullRequestConfig = {
   // By default, the merge script merges locally with `git cherry-pick` and autosquash.
   // This has the downside of pull requests showing up as `Closed` instead of `Merged`.
   // In the components repository, since we don't use fixup or squash commits, we can

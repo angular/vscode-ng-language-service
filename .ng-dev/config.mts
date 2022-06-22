4ff7a308393d6cd4de79f21b5e3aa8224159b434
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-06-22 18:06:19 +0200

    build: update ng-dev and account for ESM changes (#1701)

---
+++ .ng-dev/config.mts
@@ -0,0 +1,5 @@
+import {format} from './format.mjs';
+import {github} from './github.mjs';
+import {pullRequest} from './pull-request.mjs';
+
+export {format, github, pullRequest};
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,6 +2,8 @@
 
 set -ex -o pipefail
 
+yarn tsc --project .ng-dev/tsconfig.json
+
 yarn tslint --project client/tsconfig.json
 yarn tslint --project server/tsconfig.json
 yarn tslint --project server/src/tests/tsconfig.json

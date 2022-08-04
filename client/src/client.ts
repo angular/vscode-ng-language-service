angular/vscode-ng-language-service

commit 69fad33204474930d0fa867c5e23c82041a91dc6

build: make dist paths align with source file paths (#1733)
This is a pre-factor for migrating to Bazel

client/src/client.ts
@@ -11,9 +11,9 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 import * as lsp from 'vscode-languageclient/node';
 
-import {OpenOutputChannel, ProjectLoadingFinish, ProjectLoadingStart, SuggestStrictMode, SuggestStrictModeParams} from '../common/notifications';
-import {GetComponentsWithTemplateFile, GetTcbRequest, GetTemplateLocationForComponent, IsInAngularProject, RunNgccRequest} from '../common/requests';
-import {resolve, Version} from '../common/resolver';
+import {OpenOutputChannel, ProjectLoadingFinish, ProjectLoadingStart, SuggestStrictMode, SuggestStrictModeParams} from '../../common/notifications';
+import {GetComponentsWithTemplateFile, GetTcbRequest, GetTemplateLocationForComponent, IsInAngularProject, RunNgccRequest} from '../../common/requests';
+import {resolve, Version} from '../../common/resolver';
 
 import {isInsideComponentDecorator, isInsideInlineTemplateRegion, isInsideStringLiteral} from './embedded_support';
 

client/src/commands.ts
@@ -8,7 +8,7 @@
 
 import * as vscode from 'vscode';
 
-import {ServerOptions} from '../common/initialize';
+import {ServerOptions} from '../../common/initialize';
 
 import {AngularLanguageClient} from './client';
 import {ANGULAR_SCHEME, TcbContentProvider} from './providers';

common/requests.ts
@@ -43,7 +43,7 @@ export const RunNgccRequest =
 export interface GetTcbResponse {
   uri: lsp.DocumentUri;
   content: string;
-  selections: lsp.Range[]
+  selections: lsp.Range[];
 }
 
 export const IsInAngularProject =

esbuild.js
@@ -12,7 +12,7 @@ const defaultOptions = {
 /** @type esbuild.BuildOptions */
 const clientConfig = {
   ...defaultOptions,
-  entryPoints: ['dist/client/extension.js'],
+  entryPoints: ['dist/client/src/extension.js'],
   outfile: 'dist/npm/index.js',
   external: [
     'fs',
@@ -29,8 +29,8 @@ const clientConfig = {
 /** @type esbuild.BuildOptions */
 const bannerConfig = {
   ...defaultOptions,
-  entryPoints: ['dist/banner/banner.js'],
-  outfile: 'dist/banner/banner.esbuild.js',
+  entryPoints: ['dist/server/src/banner.js'],
+  outfile: 'dist/server/src/banner.esbuild.js',
   external: [
     'path',
   ],
@@ -43,7 +43,7 @@ const bannerConfig = {
 /** @type esbuild.BuildOptions */
 const serverConfig = {
   ...defaultOptions,
-  entryPoints: ['dist/server/server.js'],
+  entryPoints: ['dist/server/src/server.js'],
   outfile: 'dist/npm/server/index.js',
   external: [
     'fs',
@@ -61,7 +61,7 @@ async function build() {
     await esbuild.build(bannerConfig);
     await esbuild.build({
       ...serverConfig,
-      banner: {js: fs.readFileSync('dist/banner/banner.esbuild.js', 'utf8')},
+      banner: {js: fs.readFileSync('dist/server/src/banner.esbuild.js', 'utf8')},
     });
   } catch (e) {
     console.error(e);

scripts/build.sh
@@ -68,7 +68,7 @@ pushd dist/npm
 # on peer dependency conflicts (like `tslint@6` not being supported by `tslint-eslint-rules`).
 npm install --production --ignore-scripts --force
 
-sed -i -e 's#./dist/client/extension#./index#' package.json
+sed -i -e 's#./dist/client/src/extension#./index#' package.json
 ../../node_modules/.bin/vsce package
 
 popd

server/src/banner.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
 import {parseCommandLine} from './cmdline_utils';
 import {resolveTsServer} from './version_provider';
 

server/src/ngcc.ts
@@ -9,7 +9,7 @@
 import {fork} from 'child_process';
 import {dirname, resolve} from 'path';
 
-import {Version} from '../common/resolver';
+import {Version} from '../../common/resolver';
 
 import {resolveNgcc} from './version_provider';
 

server/src/session.ts
@@ -11,9 +11,9 @@ import * as ts from 'typescript/lib/tsserverlibrary';
 import {promisify} from 'util';
 import * as lsp from 'vscode-languageserver/node';
 
-import {ServerOptions} from '../common/initialize';
-import {NgccProgressEnd, OpenOutputChannel, ProjectLanguageService, ProjectLoadingFinish, ProjectLoadingStart, SuggestStrictMode} from '../common/notifications';
-import {GetComponentsWithTemplateFile, GetTcbParams, GetTcbRequest, GetTcbResponse, GetTemplateLocationForComponent, GetTemplateLocationForComponentParams, IsInAngularProject, IsInAngularProjectParams, RunNgccParams, RunNgccRequest} from '../common/requests';
+import {ServerOptions} from '../../common/initialize';
+import {NgccProgressEnd, OpenOutputChannel, ProjectLanguageService, ProjectLoadingFinish, ProjectLoadingStart, SuggestStrictMode} from '../../common/notifications';
+import {GetComponentsWithTemplateFile, GetTcbParams, GetTcbRequest, GetTcbResponse, GetTemplateLocationForComponent, GetTemplateLocationForComponentParams, IsInAngularProject, IsInAngularProjectParams, RunNgccParams, RunNgccRequest} from '../../common/requests';
 
 import {readNgCompletionData, tsCompletionEntryToLspCompletionItem} from './completion';
 import {tsDiagnosticToLspDiagnostic} from './diagnostic';

commit 8f05b5d7b73f7fc53466461d55495863fc0f38d0

build: fix dev bundle location after updating source paths
The dev bundle mapping was broken in
https://github.com/angular/vscode-ng-language-service/commit/69fad33204474930d0fa867c5e23c82041a91dc6
but is trivially fixed with this update.

client/src/client.ts
@@ -482,7 +482,7 @@ function getServerOptions(ctx: vscode.ExtensionContext, debug: boolean): lsp.Nod
   // Node module for the language server
   const args = constructArgs(ctx, viewEngine, vscode.workspace.isTrusted);
   const prodBundle = ctx.asAbsolutePath('server');
-  const devBundle = ctx.asAbsolutePath(path.join('dist', 'server', 'server.js'));
+  const devBundle = ctx.asAbsolutePath(path.join('dist', 'server', 'src', 'server.js'));
   // VS Code Insider launches extensions in debug mode by default but users
   // install prod bundle so we have to check whether dev bundle exists.
   const latestServerModule = debug && fs.existsSync(devBundle) ? devBundle : prodBundle;
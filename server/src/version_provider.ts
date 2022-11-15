340bc76ec2d41e7372c70133a383a3322b53c559
Andrew Scott <atscott01@gmail.com>
2022-11-15 11:25:42 -0800

    build: resolve dependency issues with built vsix (#1801)
    The built `vsix` needs `vscode-html-languageservice` and
    `vscode-languageserver-textdocument` in the node_modules of the vsix
    after https://github.com/microsoft/vscode-html-languageservice/commit/e7ae8a7170df5e721a13cee1b86e293b24eb3b20.
    These dependencies must be listed as direct dependencies in the
    `package.json` rather than `devDependencies`.

--- server/src/version_provider.ts
+++ server/src/version_provider.ts
@@ -12,7 +12,7 @@ import url from 'url';
 
 import {NodeModule, resolve, Version} from '../../common/resolver';
 
-const MIN_TS_VERSION = '4.3';
+const MIN_TS_VERSION = '4.8';
 const MIN_NG_VERSION = '13.0';
 const TSSERVERLIB = 'typescript/lib/tsserverlibrary';
 
